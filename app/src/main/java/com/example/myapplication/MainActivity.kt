a836ea6e227e655f07ef3eb5168db3a43aa3836a
Joy, Neena <neena.joy@accenture.com>
2019-06-05 23:06:22 +0530

    calculator application

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,21 +2,48 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
 
-//    var x = 5
+    //make canClickOperator to false when symbols are clicked one after another
+    var canClickOperator = true
 
     private val onClickListener = View.OnClickListener { view ->
+        var txt = (view as TextView).text
         if (calc_text_view.text == "0") {
+
+            if (txt == "*" || txt == "/" || txt == "^") {
+                return@OnClickListener
+
+            }
+            if (txt == "+" || txt == "-") {
+                canClickOperator = false
+
+            }
+
             calc_text_view.text = ""
             calc_text_view.text = calc_text_view.text.toString() + (view as TextView).text
-        } else
+        } else {
+            if (txt == "/" || txt == "*" || txt == "^" || txt == "." || txt == "+" || txt == "-") {
+                if (canClickOperator == false) {
+                    return@OnClickListener
+                }
+                canClickOperator = false
+
+            } else {
+                canClickOperator = true
+            }
+
             calc_text_view.text = calc_text_view.text.toString() + (view as TextView).text
+
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,76 +66,98 @@ class MainActivity : AppCompatActivity() {
         }
         plus_text_view.setOnClickListener(onClickListener)
         minus_text_view.setOnClickListener(onClickListener)
+        multiply_text_view.setOnClickListener(onClickListener)
+        divide_text_view.setOnClickListener(onClickListener)
+        decimal_text_view.setOnClickListener { onClickListener }
 
         equal_text_view.setOnClickListener {
-            //            var result = 0
-//            var nextBlock = ""
-//            var operation = 'a'
-//            calc_text_view.text.forEach { char ->
-//                if (char == '+') {
-//                    operation = '+'
-//                    result += nextBlock.toInt()
-//                    nextBlock = ""
-//
-//                } else if (char == '-') {
-//                    operation = '-'
-//                    if (result == 0) {
-//                        result += nextBlock.toInt()
-//                    } else {
-//                        result -= nextBlock.toInt()
-//                    }
-//                    nextBlock = ""
-//                } else {
-//
-//                    nextBlock += char
-//                }
-//
-//            }
-//            if (operation == '+') {
-//                result += nextBlock.toInt()
-//
-//            } else if (operation == '-') {
-//                result -= nextBlock.toInt()
-//            }
-////
-////
-//
-//            result_text_view.text = result.toString()
 
             var expression = calc_text_view.text
-            var nextBlock=""
-            val symbolList = arrayListOf<Char>()
-            val numbersList = arrayListOf<Int>()
-            expression.forEach { char ->
+            var nextBlock = ""
+            val symbolList: MutableList<Char> = ArrayList()
+            val numbersList: MutableList<Float> = ArrayList()
+
+
+            for (char in expression) {
                 if (char == '+' || char == '-' || char == '*' || char == '/') {
                     symbolList.add(char)
-                    numbersList.add(nextBlock.toInt())
-                    nextBlock=""
+                    numbersList.add(nextBlock.toFloat())
+                    nextBlock = ""
                 } else {
-                    nextBlock+=char
+                    nextBlock += char
 
                 }
 
             }
+            numbersList.add(nextBlock.toFloat())
+//
+//            val operationList = arrayListOf('^', '/', '*', '+', '-')
+//            var result = 0.00f
+//            for (operation in operationList) {
+//                for (symbol in symbolList) {
+//                    if (symbol == operation) {
+//                        var position = symbolList.indexOf(symbol)
+//
+//                        var firstOperand = numbersList.get(position)
+//                        var secondOperand = numbersList.get(position + 1)
+//                        //     var result = 0.00
+//
+//                        when (symbol) {
+//                            '+' -> result = firstOperand + secondOperand
+//                            '-' -> result = firstOperand - secondOperand
+//                            '*' -> result = firstOperand * secondOperand
+//                            '/' -> result = firstOperand / secondOperand
+////                            '^' -> result = Math.pow(firstOperand.toDouble(),secondOperand.)
+//                        }
+//                        numbersList.set(position, result)
+//                        numbersList.removeAt(position + 1)
+//                        symbolList.removeAt(position)
+//                    }
+//                }
+//
+//            }
+//            result_text_view.text = result.toString()
+//        }
+//
+//    }
+//}
 
-            val operationList = arrayListOf('^', '/', '*', '+', '_')
-            operationList.forEach { operation->
-                symbolList.forEach {symbol->
-                    if (symbol==operation)
-                    {
-                        var position=symbolList.indexOf(symbol)
+            val operationList = arrayListOf('^', '/', '*', '+', '-')
 
-                        numbersList.get(position)
+          //  val iterator = symbolList.listIterator()
+            var result = 0.00f
+            for (operation in operationList) {
+                for(symbol in symbolList) {
+
+                    if (symbol == operation) {
+                        var position = symbolList.indexOf(symbol)
+
+                        var firstOperand = numbersList.get(position)
+                        var secondOperand = numbersList.get(position + 1)
+                        //     var result = 0.00
+
+                        when (symbol) {
+                            '+' -> result = firstOperand + secondOperand
+                            '-' -> result = firstOperand - secondOperand
+                            '*' -> result = firstOperand * secondOperand
+                            '/' -> result = firstOperand / secondOperand
+//                            '^' -> result = Math.pow(firstOperand.toDouble(),secondOperand.)
+                        }
+                        numbersList.set(position, result)
+                        numbersList.removeAt(position + 1)
+                        symbolList.removeAt(position)
                     }
                 }
 
             }
-
+            result_text_view.text = result.toString()
         }
-
 
     }
 }
+
+
+
 
 
 
